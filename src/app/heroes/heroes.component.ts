252c5f00a824d1f65ecbf0ee5a2acd27791925e5
Ben Dagg <bdagg@csumb.edu>
2018-02-08 02:03:38 +0000

    added message service, hero service, dashboard view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,30 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm'
-  };
+  //current hero the user clicked on
+  selectedHero: Hero;
+  
+  
+  //array of every hero
+  heroes: Hero[];
 
-  constructor() { }
+  //creates heroService property and sets it to the singleton of HeroService
+  constructor(private heroService: HeroService) { 
+    
+  }
 
   ngOnInit() {
+    this.getHeroes()
   }
+  
+  //when click on a hero in the list item set that hero the selected hero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 
 }
